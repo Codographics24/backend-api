1e7e953d9ba55ca74716ff7edae419196db0d12b
chirag <chiragpgauswami@gmail.com>
2025-05-27 13:59:09 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -16,14 +16,20 @@ const PORT = process.env.PORT || 5000;
 // Initialize Socket.IO with CORS settings
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://exploreelara.com"],
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://exploreelara.com"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 
